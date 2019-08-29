b0c1f9e875b402254a376c5534f1898bd2a94ddd
Nigel Scott <nscott@asl-control.co.uk>
2019-08-29 14:39:17 +0100

    Add debug trace

--- out/src/extension.js
+++ out/src/extension.js
@@ -8,10 +8,23 @@ var vscode = require( 'vscode' ),
 // your extension is activated the very first time the command is executed
 function activate( context )
 {
+    var outputChannel;
+
+    function debug( text )
+    {
+        if( outputChannel === undefined )
+        {
+            outputChannel = vscode.window.createOutputChannel( "Close Unmodified" );
+        }
+
+        outputChannel.appendLine( new Date().toISOString() + ": " + text );
+    }
+
     var disposable = vscode.commands.registerCommand( 'extension.closeUnmodifiedEditors', function()
     {
         function abort()
         {
+            debug( "abort" );
             if( openEditorsTracker )
             {
                 openEditorsTracker.dispose();
@@ -20,25 +33,32 @@ function activate( context )
 
         var openEditorsTracker = vscode.window.onDidChangeActiveTextEditor( function()
         {
+            debug( "editor changed" );
+
             function next( editor )
             {
                 if( editor === undefined || editor.document.uri.path != activePath )
                 {
+                    debug( "can't do anything with this editor - trying next..." );
                     clearTimeout( aborter );
                     aborter = setTimeout( abort, 1000 );
                     vscode.commands.executeCommand( "workbench.action.nextEditor" );
                 }
                 else
                 {
+                    debug( "resetting aborter" );
                     clearTimeout( aborter );
                     openEditorsTracker.dispose();
 
+                    debug( "openEditors:" + openEditors.length );
                     if( openEditors.length > 0 )
                     {
+                        debug( "Saving editors..." );
                         vscode.workspace.saveAll( true ).then( function()
                         {
                             var keepEditors = openEditors.filter( function( editor )
                             {
+                                debug( " Checking " + editor.fileName + "..." );
                                 var keep = editor.isDirty === true;
 
                                 if( keep === false )
@@ -58,13 +78,18 @@ function activate( context )
                                     {
                                     }
                                 }
+                                debug( "   Keep: " + ( keep ? "yes" : "no" ) );
                                 return keep;
                             } );
 
+                            debug( "Closing Editors..." );
+
                             vscode.commands.executeCommand( "workbench.action.closeAllEditors" ).then( function()
                             {
+                                debug( " Reopening editors..." );
                                 keepEditors.map( function( editor )
                                 {
+                                    debug( "  Opening:" + editor.fileName );
                                     vscode.workspace.openTextDocument( editor.fileName ).then( function( document )
                                     {
                                         vscode.window.showTextDocument( document, { preview: false } );
@@ -78,15 +103,21 @@ function activate( context )
 
             if( activePath === undefined )
             {
+                debug( "setting current editor" );
                 activePath = vscode.window.activeTextEditor.document.uri.path;
             }
 
             var editor = vscode.window.activeTextEditor;
             if( editor && editor.document && editor.document.uri.scheme === 'file' )
             {
+                debug( "adding editor: " + editor.document.fileName );
                 openEditors.push( { fileName: editor.document.fileName, isDirty: editor.document.isDirty } );
+            } else
+            {
+                debug( "ignoring editor" );
             }
 
+            debug( "next editor" );
             next( editor );
         } );
 
@@ -99,9 +130,12 @@ function activate( context )
 
         var openEditors = [];
 
+        debug( "Starting..." );
         var aborter = setTimeout( abort, 1000 );
         vscode.commands.executeCommand( "workbench.action.nextEditor" );
     } );
+
+    debug( "Ready" );
 
     context.subscriptions.push( disposable );
 }
